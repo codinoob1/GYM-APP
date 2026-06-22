485e33cc980ea69b147c0a56f24d708d18590fe9
Sahilq <sak41327@gmail.com>
2026-06-22 11:15:57 +0530

    Everything is working

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold flex align-center justify-center">All start's with Hello World </h1>
+    <>
+      <h1 className="text-3xl font-bold flex align-center justify-center">All start's with Hello World </h1>
+      <h2>Design will get ready in 2-3 days</h2>
+    </>
   );
 }
