ec7974ed1578ea492cc7836a4d0c08327026f6e1
Sahilq <sak41327@gmail.com>
2026-07-09 11:42:17 +0530

    fix: case rename Dashboard to dashboard

--- src/components/PwaRegister.jsx
+++ src/components/PwaRegister.jsx
@@ -4,13 +4,9 @@ import { useEffect } from 'react';
 
 export default function PwaRegister() {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
+    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js');
-    }
-    if(process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js');
-    }
-  }, []);
+    }  }, []);
 
   return null;
 }
